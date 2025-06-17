2e70838799fb8e6a6b1daa797b6fd30e8caacdef
Linus Jungemann <38974033+LinusJungemann@users.noreply.github.com>
2025-06-17 11:20:33 +0200

    Minimum example

--- main.cpp
+++ main.cpp
@@ -8,19 +8,12 @@ namespace po = boost::program_options;
 
 int main(int argc, char* argv[]){
     // Command Line Argument Parser
-        po::options_description desc{"Options"};
-        // clang-format off
-        desc.add_options()("help,h", "Display help")("exec_mode,e", po::value<std::string>()->default_value("throughput"),
-            R"(Please select functional verification ("execute") or throughput test ("throughput")")("configpath,c", po::value<std::string>(),
-                "Required: Path to the config.json file emitted by the FINN compiler")(
-                    "input,i", po::value<std::vector<std::string>>()->multitoken()->composing(), "Path to one or more input files (npy format). Only required if mode is set to \"file\"")(
-                        "output,o", po::value<std::vector<std::string>>()->multitoken()->composing(), "Path to one or more output files (npy format). Only required if mode is set to \"file\"")(
-                            "batchsize,b", po::value<int>()->default_value(1), "Number of samples for inference")("check", "Outputs the compile time configuration");
-        // clang-format on
-        po::variables_map varMap;
-        po::store(po::parse_command_line(argc, argv, desc), varMap);
+    po::options_description desc{"Options"};
+    desc.add_options()("help,h", "Display help");
+    po::variables_map varMap;
+    po::store(po::parse_command_line(argc, argv, desc), varMap);
 
     std::cout << "Pre initialize Device" << std::endl;
-    device = xrt::device(0);
+    auto device = xrt::device(0);
     std::cout << "Post initialize Device" << std::endl;
 }
---
+++ modules.sh
@@ -0,0 +1,5 @@
+ml fpga
+ml xilinx/xrt/2.16
+ml compiler/GCC/11.3.0
+ml devel/Boost/1.79.0-GCC-11.3.0
+ml devel/CMake/3.23.1-GCCcore-11.3.0
